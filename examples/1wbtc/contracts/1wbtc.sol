be6de0ee0b1c15719bc1229957ed2559f03df6f5
Leo Chen <leo@harmony.one>
2020-08-06 12:45:55 +0000

    [1wbtc] init version of 1wbtc
    Signed-off-by: Leo Chen <leo@harmony.one>

---
+++ examples/1wbtc/contracts/1wbtc.sol
@@ -0,0 +1,22 @@
+pragma solidity >=0.4.21 <0.6.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20Detailed.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20Mintable.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20Burnable.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20Pausable.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/ownership/Ownable.sol";
+
+contract ONEWBTC is ERC20, ERC20Detailed("One Wrapped BTC", "1wBTC", 8), ERC20Mintable, ERC20Burnable, ERC20Pausable, Ownable {
+    function burn(uint value) public onlyOwner {
+        super.burn(value);
+    }
+
+    function finishMinting() public onlyOwner returns (bool) {
+        return false;
+    }
+
+    function renounceOwnership() public onlyOwner {
+        revert("renouncing ownership is blocked");
+    }
+}
